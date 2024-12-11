d608721f636240f95485fcba22accb5cfa6b8156
Juanita Benjamin <JuanitaBenjamin98@gmail.com>
2024-12-11 16:58:55 -0500

    added TMPro

---
+++ Assets/Scripts/ParticipantLog.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ParticipantLog : MonoBehaviour
+{
+    public TMP_InputField PID;
+    public TMP_Dropdown cohort_dropdown, gender_dropdown, race_dropdown;
+    public Toggle embodiment, vhInstructor;
+    public bool isIntructor, isEmbodied;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
